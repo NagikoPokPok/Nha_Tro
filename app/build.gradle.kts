fbe787f711adc0fa27f145adba226edd77d3e1dc
NagikoPokPok <119666154+NagikoPokPok@users.noreply.github.com>
2024-02-27 21:52:01 +0700

    xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "edu.poly.nhtr"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.poly.nhtr"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
