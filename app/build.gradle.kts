18376c52a38ab9db75cab16cd608eb450cd67e6c
BuiThanhNghiaDTU19122004 <119666154+NagikoPokPok@users.noreply.github.com>
2024-03-04 20:15:40 +0700

    Them dang nhap bang gmail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,14 +38,20 @@ android {
 
 dependencies {
 
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore:24.10.3")
+    implementation ("com.google.firebase:firebase-auth:22.3.1")
+
+    // Firebase gmail authentication
+    implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore:24.10.2")
 }
--- app/src/main/java/edu/poly/nhtr/Activity/SignInActivity.java
+++ app/src/main/java/edu/poly/nhtr/Activity/SignInActivity.java
@@ -5,32 +5,69 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import edu.poly.nhtr.R;
 import edu.poly.nhtr.databinding.ActivitySignInBinding;
 import edu.poly.nhtr.utilities.Constants;
 import edu.poly.nhtr.utilities.PreferenceManager;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
     private PreferenceManager preferenceManager;
-
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    GoogleSignInClient googleSignInClient;
+    int RC_SIGN_IN = 20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         preferenceManager = new PreferenceManager(getApplicationContext());
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        binding.ButtonSignInByGmail.setOnClickListener(v -> googleSignIn());
+
+        if (auth.getCurrentUser() != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
             finish();
         }
-        binding = ActivitySignInBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        setListeners();
     }
 
     private void setListeners() {
@@ -83,5 +120,59 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
+    // Phần đăng nhập bằng tài khoản Google
+    public void googleSignIn() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount googleSignInAccount = task.getResult(ApiException.class);
+
+                if (googleSignInAccount != null) {
+                    firebaseAuth(googleSignInAccount.getIdToken());
+                } else {
+                    Toast.makeText(this, "Google Sign-In failed", Toast.LENGTH_SHORT).show();
+                }
+            } catch (ApiException e) {
+                Toast.makeText(this, "Google Sign-In failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(this, "An unexpected error occurred", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    public void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("id", user.getUid());
+                        map.put("name", user.getDisplayName());
+                        map.put("profile", user.getPhotoUrl().toString());
+
+                        database.getReference().child("users").child(user.getUid()).setValue(map);
+
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Có sự cố xảy ra khi đăng nhập", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
