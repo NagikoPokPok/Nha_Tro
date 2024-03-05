39e0014688d70bddabcd35f273294d5cabafd485
BuiThanhNghiaDTU19122004 <161110817+BuiThanhNghiaDTU19122004@users.noreply.github.com>
2024-03-05 15:09:13 +0700

    Sua xml sign in va sign up

--- app/src/main/java/edu/poly/nhtr/Activity/MainActivity.java
+++ app/src/main/java/edu/poly/nhtr/Activity/MainActivity.java
@@ -4,16 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 import edu.poly.nhtr.R;
+import edu.poly.nhtr.utilities.Constants;
+import edu.poly.nhtr.utilities.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
     // Other code...
 
+    private PreferenceManager preferenceManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +36,34 @@ public class MainActivity extends AppCompatActivity {
 
     // Other methods...
 
+    public void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
     public void logout() {
-        // Add code to perform logout actions, such as clearing session data, signing out, etc.
-        // For example, if you're using Firebase:
-        FirebaseAuth.getInstance().signOut();
 
-        // Start the SignInActivity after logout
-        Intent intent = new Intent(this, SignInActivity.class);
+//
+//        showToast("Singing out ...");
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        DocumentReference documentReference =
+//                database.collection(Constants.KEY_COLLECTION_USERS).document(
+//                        preferenceManager.getString(Constants.KEY_USER_ID)
+//                );
+//        HashMap<String, Object> updates = new HashMap<>();
+//        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+//        documentReference.update(updates)
+//                .addOnSuccessListener(unused -> {
+//                    preferenceManager.clear();
+//                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+//                    finish();
+//                })
+//                .addOnFailureListener(e-> showToast("Unable to sign out"));
+
+
+        // Đăng xuất tài khoản Google
+        FirebaseAuth.getInstance().signOut();
+        // Mở trang đăng nhập
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
         startActivity(intent);
-        finish(); // Optionally, you can finish the MainActivity to prevent going back to it.
+        finish();
     }
 }
