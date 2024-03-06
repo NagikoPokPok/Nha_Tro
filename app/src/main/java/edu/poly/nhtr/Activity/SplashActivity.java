55ab76476be0683d022e0e0b1bf08e7ff068c847
Trần Quang Hiếu <tranquanghieunvl.2018@gmail.com>
2024-03-06 22:12:13 +0700

    splash screen lần 2

--- app/src/main/java/edu/poly/nhtr/Activity/SplashActivity.java
+++ app/src/main/java/edu/poly/nhtr/Activity/SplashActivity.java
@@ -1,14 +1,49 @@
 package edu.poly.nhtr.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
 import edu.poly.nhtr.R;
+import edu.poly.nhtr.databinding.ActivitySignUpBinding;
+import edu.poly.nhtr.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim, leftAnim, rightAnim;
+
+    ActivitySplashBinding binding;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        binding.txtNameApp.setAnimation(topAnim);
+        binding.txtExplore.setAnimation(topAnim);
+        binding.txtFind.setAnimation(topAnim);
+        binding.btnStart.setAnimation(bottomAnim);
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
