9f03d0b70a6083f726aa4b891602f1ea00ce249b
Trần Quang Hiếu <tranquanghieunvl.2018@gmail.com>
2024-03-06 21:33:02 +0700

    splash screen

---
+++ app/src/main/java/edu/poly/nhtr/Activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package edu.poly.nhtr.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.poly.nhtr.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
