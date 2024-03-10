fb31eadf39db92e4a979b8e9165aaa8e3d0f59c5
VoHungCuong24 <161110939+VoHungCuong24@users.noreply.github.com>
2024-03-10 09:20:16 +0700

    ForgotPass

--- app/src/main/java/edu/poly/nhtr/Activity/ForgotPasswordActivity.java
+++ app/src/main/java/edu/poly/nhtr/Activity/ForgotPasswordActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,24 +24,23 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     Button setPassword;
 
 
-    Button back;
-    TextView email;
+    ImageView back;
+    TextView email,warning;
     ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
         setPassword = findViewById(R.id.btn_DatLaiMK);
-        email = findViewById(R.id.txt_EmailQuenMK);
-        back = findViewById(R.id.btn_Back);
+        email = findViewById(R.id.edt_emailForgotPassword);
+        back = findViewById(R.id.img_back);
+        warning = findViewById(R.id.txt_warning);
         progressDialog = new ProgressDialog(this);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(ForgotPasswordActivity.this, SignInActivity.class);
-                startActivity(myIntent);
-                finish();
+                onBackPressed();
             }
         });
         setPassword.setOnClickListener(new View.OnClickListener() {
@@ -54,6 +54,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                 String[] strings = emailAddress.split("@");
                 if(strings.length!=2 || !strings[1].equals("@gmail.com")) {
                     progressDialog.dismiss();
+                    warning.setText("x Email không hợp lệ");
                     Toast.makeText(ForgotPasswordActivity.this, "Email không hợp lệ", Toast.LENGTH_SHORT).show();
                 }else{
                     FirebaseFirestore databse = FirebaseFirestore.getInstance();
@@ -77,6 +78,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                                 }
                                 else{
                                     progressDialog.dismiss();
+                                    warning.setText("x Tài khoản chưa được đăng kí");
                                     Toast.makeText(ForgotPasswordActivity.this, "Email không tồn tại", Toast.LENGTH_SHORT).show();
                                 }
 
