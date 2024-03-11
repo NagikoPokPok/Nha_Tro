cff5d737093862433b581d7eb684ab6c6645f6ec
BuiThanhNghiaDTU19122004 <161110817+BuiThanhNghiaDTU19122004@users.noreply.github.com>
2024-03-11 11:31:18 +0700

    Giao diện cài đặt

---
+++ app/src/main/java/edu/poly/nhtr/Activity/MotelManagement_Adding.java
@@ -0,0 +1,16 @@
+package edu.poly.nhtr.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.poly.nhtr.R;
+
+public class MotelManagement_Adding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motel_management_adding);
+    }
+}
---
+++ app/src/main/java/edu/poly/nhtr/Activity/SettingsActivity.java
@@ -0,0 +1,16 @@
+package edu.poly.nhtr.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.poly.nhtr.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
