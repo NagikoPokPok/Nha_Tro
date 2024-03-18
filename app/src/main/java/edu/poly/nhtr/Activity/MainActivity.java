11e770792fdeaea5a03dea1486d25f95e0d0af46
DucHuyH <huynhduchuy47@gmail.com>
2024-03-18 23:51:51 +0700

    Button press + update profile(name+image)

--- app/src/main/java/edu/poly/nhtr/Activity/MainActivity.java
+++ app/src/main/java/edu/poly/nhtr/Activity/MainActivity.java
@@ -4,29 +4,16 @@ package edu.poly.nhtr.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Objects;
 
 import edu.poly.nhtr.R;
 import edu.poly.nhtr.databinding.ActivityMainBinding;
@@ -37,28 +24,52 @@ import edu.poly.nhtr.utilities.PreferenceManager;
 public class MainActivity extends AppCompatActivity {
     // Other code...
     Button changeProfile;
+    private String encodedImage;
     PreferenceManager preferenceManager;
     private ActivityMainBinding binding;
-    private String encodedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
         preferenceManager = new PreferenceManager(getApplicationContext());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+        loadUserDetails();
 
+    }
+
+    private String encodedImage(Bitmap bitmap) // Hàm mã hoá ảnh thành chuỗi Base64
+    {
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() + previewWidth / bitmap.getWidth();
+        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    private void loadUserDetails(){
+        binding.name.setText(preferenceManager.getString(Constants.KEY_NAME));
+        try {
+
+            byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            encodedImage = encodedImage(bitmap);
+
+            byte[] encodedBytes = Base64.decode(encodedImage(bitmap), Base64.DEFAULT);
+            Bitmap encodedBitmap = BitmapFactory.decodeByteArray(encodedBytes, 0, encodedBytes.length);
+            binding.imageProfile.setImageBitmap(encodedBitmap);
+
+            binding.txtAddImage.setVisibility(View.INVISIBLE);
+        }catch (Exception e){
+            Toast.makeText(this, "Không thể tải ảnh", Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void setListeners() {
         openSettings();
-        GoogleSignInAccount googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
-        if (googleSignInAccount != null) {
-            getInfoFromGoogle();
-        } else {
-            loadUserDetail();
-        }
     }
 
     private void openSettings() {
@@ -71,91 +82,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    private String encodedImage(Bitmap bitmap) // Hàm mã hoá ảnh thành chuỗi Base64
-    {
-        // d
-        int previewWidth = 150;
-        int previewHeight = bitmap.getHeight() + previewWidth / bitmap.getWidth();
-        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        return Base64.encodeToString(bytes, Base64.DEFAULT);
-    }
-    private void loadUserDetail(){
-
-        try {
-
-            byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
-            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-            encodedImage = encodedImage(bitmap);
-
-            byte[] encodedBytes = Base64.decode(encodedImage(bitmap), Base64.DEFAULT);
-            Bitmap encodedBitmap = BitmapFactory.decodeByteArray(encodedBytes, 0, encodedBytes.length);
-            binding.imgProfile.setImageBitmap(encodedBitmap);
-
-            binding.txtAddImage.setVisibility(View.INVISIBLE);
-        }catch (Exception e){
-            Toast.makeText(this, "Không thể tải ảnh", Toast.LENGTH_SHORT).show();
-        }
-
-        binding.name.setText(preferenceManager.getString(Constants.KEY_NAME));
-
-    }
-
-
-    // Lấy ảnh đại diện và tên từ Google
-    private void getInfoFromGoogle() {
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if (account != null) {
-            String userName = account.getDisplayName();
-            binding.name.setText(userName);
-
-            String photoUrl = Objects.requireNonNull(account.getPhotoUrl()).toString();
-            new DownloadImageTask(binding.imgProfile).execute(photoUrl);
-
-            binding.txtAddImage.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        private ImageView imageView;
-
-        public DownloadImageTask(ImageView imageView) {
-            this.imageView = imageView;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String imageUrl = urls[0];
-            Bitmap bitmap = null;
-            try {
-                InputStream in = new java.net.URL(imageUrl).openStream();
-                bitmap = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return bitmap;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            if (result != null) {
-                imageView.setImageBitmap(result);
-            }
-        }
-    }
-//
-//    private String encodedImage(Bitmap bitmap) // Hàm mã hoá ảnh thành chuỗi Base64
-//    {
-//        int previewWidth = 150;
-//        int previewHeight = bitmap.getHeight() + previewWidth / bitmap.getWidth();
-//        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
-//        byte[] bytes = byteArrayOutputStream.toByteArray();
-//        return Base64.encodeToString(bytes, Base64.DEFAULT);
-//    }
+    // Other methods...
 
     public void showToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
