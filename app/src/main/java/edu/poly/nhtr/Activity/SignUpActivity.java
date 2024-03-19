f8ec6d41f0bc8af4ea5d5fbffa0b87819914e548
Trần Quang Hiếu <tranquanghieunvl.2018@gmail.com>
2024-03-19 08:23:53 +0700

    xoá

--- app/src/main/java/edu/poly/nhtr/Activity/SignUpActivity.java
+++ app/src/main/java/edu/poly/nhtr/Activity/SignUpActivity.java
@@ -26,8 +26,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.appcheck.FirebaseAppCheck;
-import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
