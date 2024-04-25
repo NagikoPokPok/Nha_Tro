43cf40122d3227017e1aa884a35cb5e3ed54ef3b
VoHungCuong24 <161110939+VoHungCuong24@users.noreply.github.com>
2024-04-25 22:43:41 +0700

    sửa lại forgotPassword

---
+++ app/src/main/java/edu/poly/nhtr/Activity/Service/ServiceMainActivity.java
@@ -0,0 +1,16 @@
+package edu.poly.nhtr.Activity.Service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.poly.nhtr.R;
+
+public class ServiceMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_main);
+    }
+}
