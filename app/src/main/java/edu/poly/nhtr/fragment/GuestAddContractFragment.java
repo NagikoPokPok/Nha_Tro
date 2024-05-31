622b2eed8c59e8d04c19b5ffdc5b5949a0874b0e
BuiThanhNghiaDTU19122004 <buithanhnghiadn2004@gmail.com>
2024-05-31 09:15:28 +0700

    UI hợp đồng khác thuê
    Signed-off-by: BuiThanhNghiaDTU19122004 <buithanhnghiadn2004@gmail.com>

---
+++ app/src/main/java/edu/poly/nhtr/fragment/GuestAddContractFragment.java
@@ -0,0 +1,21 @@
+package edu.poly.nhtr.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.poly.nhtr.R;
+
+public class GuestAddContractFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_guest_add_contract, container, false);
+    }
+}
