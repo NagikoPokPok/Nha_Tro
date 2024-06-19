73a7368bbf676ba7632d0ecb899a7667f086166c
BuiThanhNghiaDTU19122004 <buithanhnghiadn2004@gmail.com>
2024-06-19 10:26:04 +0700

    Sửa khung giới tính và xử lý code giới tính và ẩn hint ngày sinh, sửa cỡ chữ và khỏang cách giữa cách thành phần trong fragment_guest_add_contract.xml
    Signed-off-by: BuiThanhNghiaDTU19122004 <buithanhnghiadn2004@gmail.com>

--- app/src/main/java/edu/poly/nhtr/fragment/GuestAddContractFragment.java
+++ app/src/main/java/edu/poly/nhtr/fragment/GuestAddContractFragment.java
@@ -1,21 +1,81 @@
 package edu.poly.nhtr.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import edu.poly.nhtr.R;
+import edu.poly.nhtr.databinding.FragmentGuestAddContractBinding;
 
 public class GuestAddContractFragment extends Fragment {
 
+    private FragmentGuestAddContractBinding binding;
+    private TextInputLayout tilNgaySinh;
+    private TextInputEditText edtNgaySinh;
+    private AutoCompleteTextView edtGioiTinh;
+    ArrayAdapter<String> adapterItems;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guest_add_contract, container, false);
+        binding = FragmentGuestAddContractBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initializeViews();
+        setUpNgaySinhField();
+        setUpDropDownMenuGender();
+    }
+
+    private void initializeViews() {
+        tilNgaySinh = binding.tilNgaySinh;
+        edtNgaySinh = binding.edtNgaySinh;
+        edtGioiTinh = binding.edtGioiTinh;
+    }
+
+    private void setUpNgaySinhField() {
+        edtNgaySinh.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                tilNgaySinh.setHint("");
+            } else {
+                tilNgaySinh.setHint(getString(R.string.dd_mm_yyyy));
+            }
+        });
+    }
+
+    // Hàm xử lý menu dropdown của giới tính
+    private void setUpDropDownMenuGender() {
+        String[] genderOptions = getResources().getStringArray(R.array.gender_options);
+        adapterItems = new ArrayAdapter<>(requireContext(), R.layout.custom_dropdown_item, genderOptions);
+        edtGioiTinh.setAdapter(adapterItems);
+
+
+
+//        // Xử lý sự kiện khi chọn giới tính
+//        edtGioiTinh.setOnItemClickListener((parent, view, position, id) -> {
+//            String selectedGender = (String) parent.getItemAtPosition(position);
+//
+//        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
