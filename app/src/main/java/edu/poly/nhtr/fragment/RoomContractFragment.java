5fed0a69bf23c698c013389a6d3ffb0402f061e8
Trần Quang Hiếu <tranquanghieunvl.2018@gmail.com>
2024-08-01 20:07:03 +0700

    adding cancel repetitive in RoomContractFragment

--- app/src/main/java/edu/poly/nhtr/fragment/RoomContractFragment.java
+++ app/src/main/java/edu/poly/nhtr/fragment/RoomContractFragment.java
@@ -1,6 +1,9 @@
 package edu.poly.nhtr.fragment;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,12 +12,17 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 import edu.poly.nhtr.R;
+import edu.poly.nhtr.alarmManager.AlarmService;
 import edu.poly.nhtr.databinding.FragmentRoomContractBinding;
 import edu.poly.nhtr.interfaces.RoomContractInterface;
 import edu.poly.nhtr.models.Home;
@@ -38,6 +46,10 @@ public class RoomContractFragment extends Fragment implements RoomContractInterf
     private String roomPrice;
     private Home home;
     private OnFragmentInteractionListener listener;
+    private Dialog dialog;
+    private AlarmService alarmService;
+    private AlarmService alarmService2;
+    private String header1, body1, header2, body2;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -62,12 +74,21 @@ public class RoomContractFragment extends Fragment implements RoomContractInterf
         super.onViewCreated(view, savedInstanceState);
         preferenceManager = new PreferenceManager(requireContext());
         presenter = new RoomContractPresenter(this);
+        dialog = new Dialog(requireContext());
 
         if (getArguments() != null) {
             room = (Room) getArguments().getSerializable("room");
             roomPrice = getArguments().getString("room_price");
             home = (Home) getArguments().getSerializable("home");
             if (room != null) {
+                header1 = "Sắp tới ngày gửi hoá đơn cho phòng " + room.getNameRoom() + " tại nhà trọ " + home.getNameHome();
+                body1 = "Bạn cần lập hoá đơn tháng này cho phòng " + room.getNameRoom() + " tại nhà trọ " + home.getNameHome();
+
+                header2 = "Đã tới ngày gửi hoá đơn cho phòng " + room.getNameRoom() + " tại nhà trọ " + home.getNameHome();
+                body2 = "Bạn cần gửi hoá đơn tháng này cho phòng " + room.getNameRoom() + " tại nhà trọ " + home.getNameHome();
+
+                alarmService = new AlarmService(requireContext(), home, room, header1, body1);
+                alarmService2 = new AlarmService(requireContext(), home, room, header2, body2);
                 Log.d("RoomContractFragment", "Room ID: " + room.getRoomId());
                 setListeners();
             } else {
@@ -116,9 +137,9 @@ public class RoomContractFragment extends Fragment implements RoomContractInterf
         });
         binding.btnPrintContract.setOnClickListener(v -> printContract());
         binding.btnDeleteContract.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onHideTabLayoutAndViewPager();
-            }
+//            if (listener != null) {
+//                listener.onHideTabLayoutAndViewPager();
+//            }
             deleteContract();
         });
     }
@@ -165,7 +186,59 @@ public class RoomContractFragment extends Fragment implements RoomContractInterf
     }
 
     private void deleteContract() {
-        presenter.deleteContract(room);
+        openConfirmDeleteContractDialog();
+
+    }
+
+    private void openConfirmDeleteContractDialog() {
+        setupDialog(R.layout.layout_dialog_delete_contract);
+
+        Button btnCancel = dialog.findViewById(R.id.btn_cancel);
+        Button btnDeleteContract = dialog.findViewById(R.id.btn_confirm_delete_contract);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnDeleteContract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelRepetitiveAlarm();
+                presenter.deleteContract(room);
+            }
+        });
+    }
+
+    public void cancelRepetitiveAlarm()
+    {
+        showToast("Delete contract => Cancel alarm");
+        int requestCode1 = Integer.parseInt(preferenceManager.getString(Constants.KEY_NOTIFICATION_REQUEST_CODE, room.getRoomId()+"code1"));
+        alarmService.cancelRepetitiveAlarm(requestCode1);
+
+        int requestCode2 = Integer.parseInt(preferenceManager.getString(Constants.KEY_NOTIFICATION_REQUEST_CODE, room.getRoomId()+"code2"));
+        alarmService2.cancelRepetitiveAlarm(requestCode2);
+    }
+
+    private void setupDialog(int layoutId) {
+        dialog.setContentView(layoutId);
+        Window window = dialog.getWindow();
+        if (window != null) {
+            window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            WindowManager.LayoutParams windowAttributes = window.getAttributes();
+            windowAttributes.gravity = Gravity.CENTER;
+            window.setAttributes(windowAttributes);
+            dialog.setCancelable(true);
+            dialog.show();
+        }
+    }
+
+    public void showButtonLoading(int id) {
+        dialog.findViewById(id).setVisibility(View.INVISIBLE);
+        dialog.findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
     }
 
     @Override
